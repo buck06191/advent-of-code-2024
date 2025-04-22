bd98fd9a88b58fee53f86314b33fbda0c83443cb
Josh Buckland <josh.russell.buckland@gmail.com>
2025-04-22 12:14:37 +0100

    feat: implement part two

--- day-3/index.ts
+++ day-3/index.ts
@@ -1,53 +1,93 @@
-export type MemoryInstruction = {
-  operation: string;
+export type EnableInstruction = {
+  operation: "do";
+};
+
+export type DisableInstruction = {
+  operation: "don't";
+};
+
+export type MultiplicationInstruction = {
+  operation: "mul";
   num1: number;
   num2: number;
 };
 
-export function parseMemory(input: string): MemoryInstruction[] {
-  const pattern = /(?<operation>mul)\((?<num1>\d+),(?<num2>\d+)\)/g;
+export type MemoryInstruction =
+  | EnableInstruction
+  | DisableInstruction
+  | MultiplicationInstruction;
 
-  return [...input.matchAll(pattern)]
-    .map(({ groups }) => {
+export function parseMemory(
+  input: string,
+  day: "partOne" | "partTwo",
+): MemoryInstruction[] {
+  const pattern =
+    day === "partOne"
+      ? /(?<operation>mul)\((?<numbers>\d+,\d+)\)/g
+      : /(?<operation>mul|do|don't)\((?<numbers>\d+,\d+)*\)/g;
+
+  const possibleInstructions = [...input.matchAll(pattern)].map(
+    ({ groups }) => {
       if (!groups) {
         return undefined;
       }
 
-      return {
-        operation: groups.operation,
-        num1: parseInt(groups.num1),
-        num2: parseInt(groups.num2),
-      };
-    })
-    .filter((instruction) => !!instruction);
-}
+      switch (groups.operation) {
+        case "mul": {
+          const numbers = groups.numbers;
+          const [num1, num2] = numbers.split(",").map((num) => parseInt(num));
 
-export function processInstruction({
-  operation,
-  num1,
-  num2,
-}: MemoryInstruction): number {
-  switch (operation) {
-    case "mul":
-      return num1 * num2;
-    default:
-      return 0;
-  }
+          return {
+            operation: groups.operation,
+            num1,
+            num2,
+          };
+        }
+        case "do":
+        case "don't":
+          return { operation: groups.operation };
+        default:
+          throw new Error(`Unexpected Operation: ${groups.operation}`);
+      }
+    },
+  );
+
+  return possibleInstructions.filter((instruction) => !!instruction);
 }
 
 export function processInstructions(instructions: MemoryInstruction[]): number {
+  let isEnabled = true;
   return instructions.reduce((prevResult, currentInstruction) => {
-    return prevResult + processInstruction(currentInstruction);
+    switch (currentInstruction.operation) {
+      case "do": {
+        isEnabled = true;
+        return prevResult;
+      }
+      case "don't": {
+        isEnabled = false;
+        return prevResult;
+      }
+      case "mul": {
+        return (
+          prevResult +
+          (isEnabled ? currentInstruction.num1 * currentInstruction.num2 : 0)
+        );
+      }
+    }
   }, 0);
 }
 
 export async function main(filePath: string) {
-  console.log("##### Part One #####");
   const input = await Deno.readTextFile(filePath);
 
-  const instructions = parseMemory(input);
-  const output = processInstructions(instructions);
-  console.log(`Output is ${output}`);
+  console.log("##### Part One #####");
+  const instructionsOne = parseMemory(input, "partOne");
+  const outputOne = processInstructions(instructionsOne);
+  console.log(`Output is ${outputOne}`);
 
   console.log("##### Part Two #####");
+
+  const instructionsTwo = parseMemory(input, "partTwo");
+  const outputTwo = processInstructions(instructionsTwo);
+  console.log(`Output is ${outputTwo}`);
 }
--- day-3/index_test.ts
+++ day-3/index_test.ts
@@ -2,56 +2,84 @@ import { beforeEach, describe, it } from "jsr:@std/testing/bdd";
 import {
   MemoryInstruction,
   parseMemory,
-  processInstruction,
   processInstructions,
 } from "./index.ts";
 import { assertEquals } from "@std/assert/equals";
 
-const input =
+const partOneInput =
   "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+const partTwoInput =
+  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
 describe("parseMemory", () => {
-  let actual: MemoryInstruction[];
+  describe("partOne", () => {
+    let actual: MemoryInstruction[];
 
-  beforeEach(() => {
-    actual = parseMemory(input);
+    beforeEach(() => {
+      actual = parseMemory(partOneInput, "partOne");
+    });
+    it("should extract the correct instructions", () => {
+      assertEquals(actual, [
+        { operation: "mul", num1: 2, num2: 4 },
+        { operation: "mul", num1: 5, num2: 5 },
+        { operation: "mul", num1: 11, num2: 8 },
+        { operation: "mul", num1: 8, num2: 5 },
+      ]);
+    });
   });
-  it("should extract the correct instructions", () => {
-    assertEquals(actual, [
-      { operation: "mul", num1: 2, num2: 4 },
-      { operation: "mul", num1: 5, num2: 5 },
-      { operation: "mul", num1: 11, num2: 8 },
-      { operation: "mul", num1: 8, num2: 5 },
-    ]);
-  });
-});
 
-describe("processInstruction", () => {
-  const inputs = [
-    { instruction: { operation: "mul", num1: 2, num2: 4 }, expected: 8 },
-    { instruction: { operation: "mul", num1: 5, num2: 5 }, expected: 25 },
-    { instruction: { operation: "mul", num1: 11, num2: 8 }, expected: 88 },
-    { instruction: { operation: "mul", num1: 8, num2: 5 }, expected: 40 },
-  ];
+  describe("partTwo", () => {
+    let actual: MemoryInstruction[];
 
-  inputs.forEach(({ instruction, expected }) => {
-    it("should process the instruction correctly", () => {
-      const actual = processInstruction(instruction);
-      assertEquals(actual, expected);
+    beforeEach(() => {
+      actual = parseMemory(partTwoInput, "partTwo");
+    });
+    it("should extract the correct instructions", () => {
+      assertEquals(actual, [
+        { operation: "mul", num1: 2, num2: 4 },
+        { operation: "don't" },
+        { operation: "mul", num1: 5, num2: 5 },
+        { operation: "mul", num1: 11, num2: 8 },
+        { operation: "do" },
+        { operation: "mul", num1: 8, num2: 5 },
+      ]);
     });
   });
 });
 
-describe("processInstructions", () => {
-  const input = [
+const partOneScenario = {
+  description: "with only multiplication instructions",
+  input: [
     { operation: "mul", num1: 2, num2: 4 },
     { operation: "mul", num1: 5, num2: 5 },
     { operation: "mul", num1: 11, num2: 8 },
     { operation: "mul", num1: 8, num2: 5 },
-  ];
+  ] as MemoryInstruction[],
+  expected: 161,
+};
 
-  it("should process the instructions correctly", () => {
-    const actual = processInstructions(input);
-    assertEquals(actual, 161);
+const partTwoScenario = {
+  description: "with enablement and multiplication instructions",
+  input: [
+    { operation: "mul", num1: 2, num2: 4 },
+    { operation: "don't" },
+    { operation: "mul", num1: 5, num2: 5 },
+    { operation: "mul", num1: 11, num2: 8 },
+    { operation: "do" },
+    { operation: "mul", num1: 8, num2: 5 },
+  ] as MemoryInstruction[],
+  expected: 48,
+};
+
+const scenarios = [partOneScenario, partTwoScenario];
+
+describe("processInstructions", () => {
+  scenarios.forEach(({ description, input, expected }) => {
+    describe(description, () => {
+      it("should process the instructions correctly", () => {
+        const actual = processInstructions(input);
+        assertEquals(actual, expected);
+      });
+    });
   });
 });
